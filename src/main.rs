f3de1c08970c12f00da69f4c8887c21e444ae5d3
Maarten de Vries <maarten@de-vri.es>
2020-04-26 19:50:59 +0200

    Implement pretty-printing and checking for unbalanced transactions.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use grootboek::Account;
+use grootboek::Cents;
+use grootboek::Date;
+use grootboek::Transaction;
+use std::path::PathBuf;
 use structopt::StructOpt;
 use structopt::clap::AppSettings;
-use std::path::PathBuf;
-use grootboek::Date;
 use yansi::Paint;
 
 #[derive(StructOpt)]
@@ -12,10 +15,14 @@ struct Options {
 	/// The file to parse.
 	file: PathBuf,
 
-	/// Give a summary for one node.
+	/// Consider only transactions that mutate the given account or a sub-account.
 	#[structopt(long, short)]
-	#[structopt(value_name = "NODE")]
-	node: Option<String>,
+	#[structopt(value_name = "ACCOUNT")]
+	account: Option<String>,
+
+	/// Check for unbalanced transactions.
+	#[structopt(long, short)]
+	check: bool,
 
 	/// Limit records to this period.
 	#[structopt(long)]
@@ -102,7 +109,7 @@ fn do_main(options: &Options) -> Result<(), String> {
 	};
 
 	let data = std::fs::read_to_string(&options.file).map_err(|e| format!("failed to read {:?}: {}", options.file, e))?;
-	let transactions = grootboek::Transaction::parse_from_str(&data).map_err(|e| format!("{}", e))?;
+	let transactions = Transaction::parse_from_str(&data).map_err(|e| format!("{}", e))?;
 	let transactions = transactions.into_iter().filter(|transaction| {
 		if let Some(start_date) = &start_date {
 			if transaction.date < *start_date {
@@ -114,14 +121,42 @@ fn do_main(options: &Options) -> Result<(), String> {
 				return false;
 			}
 		}
+		if let Some(account) = &options.account {
+			if !transaction.mutates_account(account) {
+				return false;
+			}
+		}
 		true
 	});
 
-	for (i, transaction) in transactions.enumerate() {
-		if i > 0 { println!(); }
-		print_full(&transaction);
+	if options.check {
+		let mut unbalanced_transactions = 0;
+		for (transaction, balance) in find_unbalanced(transactions) {
+			print_full(&transaction);
+			println!("{prefix} {balance}",
+				prefix = Paint::red("Unbalanced amount:").bold(),
+				balance = color_cents(balance),
+			);
+			unbalanced_transactions += 1;
+			println!()
+		}
+
+		if unbalanced_transactions != 0 {
+			Err(format!("Found {} unbalanced transactions.", unbalanced_transactions))
+		} else {
+			Ok(())
+		}
+
+	} else {
+		let totals = compute_totals(transactions);
+		print_totals(&totals);
+		Ok(())
 	}
-	Ok(())
+
+	// for (i, transaction) in transactions.enumerate() {
+	// 	if i > 0 { println!(); }
+	// 	print_full(&transaction);
+	// }
 }
 
 fn main() {
@@ -131,7 +166,15 @@ fn main() {
 	}
 }
 
-fn print_full(transaction: &grootboek::Transaction) {
+fn color_cents(cents: Cents) -> impl std::fmt::Display {
+	let color = match cents.is_negative() {
+		true => yansi::Color::Red.style(),
+		false => yansi::Color::Green.style(),
+	};
+	color.paint(cents)
+}
+
+fn print_full(transaction: &Transaction) {
 	println!("{date}: {desc}",
 		date = Paint::cyan(transaction.date),
 		desc = Paint::magenta(transaction.description),
@@ -151,5 +194,95 @@ fn print_full(transaction: &grootboek::Transaction) {
 			amount  = color.paint(mutation.amount),
 			account = mutation.account,
 		);
+	}
+}
+
+struct Tree<'a, T> {
+	root: Node<'a, T>,
+}
+
+struct Node<'a, T> {
+	account: Account<'a>,
+	data: T,
+	children: Vec<Node<'a, T>>,
+}
+
+impl<'a, T> Tree<'a, T> {
+	fn new(root_data: T) -> Self {
+		Self {
+			root: Node::new(Account::from_raw(""), root_data),
+		}
+	}
+
+	fn insert(&mut self, account: Account<'a>, update: impl Fn(&mut T), initial_data: T)
+	where
+		T: Clone
+	{
+		update(&mut self.root.data);
+		let mut current = &mut self.root;
+		for account in account.walk_nodes() {
+			if let Some(x) = current.children.iter().position(|x| x.account == account) {
+				current = &mut current.children[x];
+			} else {
+				current.children.push(Node::new(account, initial_data.clone()));
+				current = current.children.last_mut().unwrap();
+			}
+			update(&mut current.data);
+		}
+	}
+}
+
+impl<'a, T> Node<'a, T> {
+	fn new(account: Account<'a>, data: T) -> Self {
+		Self { account, data, children: Vec::new() }
+	}
+}
+
+fn compute_totals<'a>(transactions: impl IntoIterator<Item = Transaction<'a>>) -> Tree<'a, Cents> {
+	let mut root = Tree::new(Cents(0));
+
+	for transaction in transactions {
+		for mutation in &transaction.mutations {
+			root.insert(mutation.account, |x| *x += mutation.amount, Cents(0));
+		}
+	}
+
+	root
+}
+
+fn find_unbalanced<'a>(transactions: impl IntoIterator<Item = Transaction<'a>>) -> impl Iterator<Item = (Transaction<'a>, Cents)> {
+	transactions.into_iter().filter_map(|transaction| {
+		let balance = transaction.mutations.iter().fold(Cents(0), |sum, mutation| sum + mutation.amount);
+		if balance != Cents(0) {
+			Some((transaction, balance))
+		} else {
+			None
+		}
+	})
+}
+
+fn print_totals(totals: &Tree<Cents>) {
+	let color = match totals.root.data.is_negative() {
+		true => yansi::Color::Red.style(),
+		false => yansi::Color::Green.style(),
+	};
+	println!("Total: {}", color.paint(totals.root.data));
+	print_totals_subtree(&totals.root, "");
+}
+
+fn print_totals_subtree(node: &Node<Cents>, indent: &str) {
+	for (i, child) in node.children.iter().enumerate() {
+		let (tree_char, subindent) = if i == node.children.len() - 1 {
+			("└─", "   ")
+		} else {
+			("├─", "│  ")
+		};
+
+		let color = match child.data.is_negative() {
+			true => yansi::Color::Red.style(),
+			false => yansi::Color::Green.style(),
+		};
+		println!("{}{} {}: {}", indent, tree_char, child.account.name(), color.paint(child.data));
+		print_totals_subtree(child, &format!("{}{}", indent, subindent));
 	}
 }
